474ffba6752880d3b39c6ea99542c8208172fcea
chris held <chris.held@zyquest.com>
2018-04-16 12:30:51 -0500

    Add flag model

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Library, type: :model do
+  # associations
+  it { should have_many(:library_flags).dependent(:destroy)}
   # validations
   it {should validate_presence_of(:lat)}
   it {should validate_presence_of(:lon)}
